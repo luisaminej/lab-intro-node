8b55cbb7011de050fef84f99b48c687e580ac47e
Luisa Mine <luisaminj@gmail.com>
2021-07-13 17:04:18 -0500

    Completado con bonus

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length
+    this.items = this.items.sort((a,b) => (a - b))
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+ if(this.items[pos]) {
+   return this.items[pos]
 
-  min() {}
+ } else {
+   throw new Error ("OutOfBounds");
+ }
 
-  sum() {}
+ }
+  max() {
+   if(this.items.length === 0){
+    throw new Error('EmptySortedList');
+  } else { 
+    return this.items[this.items.length -1]
+  }
 
-  avg() {}
+}
+
+  min() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else { 
+      return this.items[0]
+    }
+  }
+
+  sum() {
+    if(this.length !== 0) {
+      return this.items.reduce((a,b) => a + b)
+  } else {
+    return 0;
+  }
+  }
+
+  avg() {
+    if(this.items.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return ((this.items.reduce((a,b) => a + b ))/ this.length)
+
+    }
+  }
 }
 
 module.exports = SortedList;
